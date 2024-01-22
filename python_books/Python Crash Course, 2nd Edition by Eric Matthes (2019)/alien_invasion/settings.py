8badcc09d6f8e3ace00631f376e5aa70f0827b7d
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2024-01-22 14:12:24 -0300

    || Drawing the Ship to the Screen

--- python_books/Python Crash Course, 2nd Edition by Eric Matthes (2019)/alien_invasion/settings.py
+++ python_books/Python Crash Course, 2nd Edition by Eric Matthes (2019)/alien_invasion/settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
