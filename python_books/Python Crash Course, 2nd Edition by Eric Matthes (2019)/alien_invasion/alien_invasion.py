a1be9cc3d5f8971d0b4fd39d092f52b0650eb9b6
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2024-01-22 13:55:39 -0300

    || Adding the Ship Image

--- python_books/Python Crash Course, 2nd Edition by Eric Matthes (2019)/alien_invasion/alien_invasion.py
+++ python_books/Python Crash Course, 2nd Edition by Eric Matthes (2019)/alien_invasion/alien_invasion.py
@@ -10,6 +10,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
@@ -29,6 +30,7 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
