3abcd2c9aa0a4ecde660e2233508023e1908f5ef
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2024-01-22 09:22:34 -0300

    PYTHON | Alien Invasion | Basic structure

---
+++ python_books/Python Crash Course, 2nd Edition by Eric Matthes (2019)/alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Initialize the game's settings."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
