4691be95e364b4f005d44ca5c190fbca1e129a66
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2024-01-23 16:45:19 -0300

    || Firing Bullets

--- python_books/Python Crash Course, 2nd Edition by Eric Matthes (2019)/alien_invasion/alien_invasion.py
+++ python_books/Python Crash Course, 2nd Edition by Eric Matthes (2019)/alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overrall class to manage game assets and behavior."""
@@ -20,12 +21,14 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
     
@@ -47,6 +50,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_a:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -56,10 +61,17 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
         
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip()
